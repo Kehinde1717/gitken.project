42e3a40bc7fdc4bed11af771abdde8018c8c472d
Kehinde Ifarajimi <godlinessphilip@gmail.com>
2022-09-17 15:30:13 +0100

    Version 1

---
+++ config.js
@@ -0,0 +1,3 @@
+console.log('console.js');
+console.log('version2 updated');
+console.log('version3');
---
+++ test.py
@@ -0,0 +1,12 @@
+negative = "-";
+user = input('Enter number: ');
+totalUser = user;
+
+if user:
+    sum = user + totalUser;
+    if negative:
+        print("Stop");
+    else:
+        sum = user + totalUser;
+else:
+    print("Input number");
